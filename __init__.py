b88c15320190d50643a270cdd37330b05779058d
Dharm P <dharmpat@outlook.com>
2025-08-17 05:54:29 -0400

    0000_Testing_Commiter

--- __init__.py
+++ __init__.py
@@ -1,15 +1,40 @@
-difficulty = input("Difficulty? ")
-number = input("Problem Number? ")
-name = input("Problem Name? ")
+import os
+from pathlib import Path
+import subprocess
 
-def commiter(difficulty, number, name):
-    
-    #open folder leetCodeSolved
-    #open folder [difficulty]
+difficulty = input("Difficulty -  ").lower()
+num = int(input("Problem Number -  "))
+name = input("Problem Name -  ")
+solution = input("Solution- ")
+
+def commiter(difficulty, num, name, solution):
+
+    #formating the file name
+    formattedName = name.replace(" ", "_").title()
+    formattedNum = f"{num:04}"
+    fileName = f"{formattedNum}_{formattedName}"
+
+    #open folder leetCodeSolved[difficulty]
+    os.startfile(fr"C:\Users\patel\Downloads\leetCodeSolved\{difficulty}")
+
+
     #add file [problemNumber_problemNameParsed(i.e Two_Sum)]
+    #opens file and writes solution
+    with open(f"{fileName}.py", "w", encoding="utf-8") as fh:
+        fh.write("hello")
+
     #run 
         #git add .
         #git commit -m "Create [fileName]"
         #git push origin main
+        try:
+            subprocess.run(["git", "add", "."], check=True)
+            subprocess.run(["git", "commit", "-m", f"{fileName}"], check=True)
+            subprocess.run(["git", "push", "origin", "main"], check=True)
+        except subprocess.CalledProcessError as e:
+            print("Git command failed!")
+            print("Exit code:", e.returncode)
 
     return
+
+commiter(difficulty, num, name, solution)
