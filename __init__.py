9b4c773f44a05b18a16457d92e5caeab4a716819
Dharm P <dharmpat@outlook.com>
2025-08-17 05:14:01 -0400

    project skeleton

---
+++ __init__.py
@@ -0,0 +1,15 @@
+difficulty = input("Difficulty? ")
+number = input("Problem Number? ")
+name = input("Problem Name? ")
+
+def commiter(difficulty, number, name):
+    
+    #open folder leetCodeSolved
+    #open folder [difficulty]
+    #add file [problemNumber_problemNameParsed(i.e Two_Sum)]
+    #run 
+        #git add .
+        #git commit -m "Create [fileName]"
+        #git push origin main
+
+    return
